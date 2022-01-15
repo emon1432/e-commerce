439fe8c4a4b4222f8bf8dc5123e4abc7dd8ba289
emon1432 <ovirajemon143@gmail.com>
2022-01-16 01:36:02 +0600

    Starting professional E-commerce site with laravel

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Twitter -->
+    <meta name="twitter:site" content="@themepixels">
+    <meta name="twitter:creator" content="@themepixels">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Starlight">
+    <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="twitter:image" content="http://themepixels.me/starlight/img/starlight-social.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="http://themepixels.me/starlight">
+    <meta property="og:title" content="Starlight">
+    <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
+
+    <meta property="og:image" content="http://themepixels.me/starlight/img/starlight-social.png">
+    <meta property="og:image:secure_url" content="http://themepixels.me/starlight/img/starlight-social.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+
+    <!-- Meta -->
+    <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="author" content="ThemePixels">
+
+    <title>Admin Panel Registration</title>
+
+    <!-- vendor css -->
+    <link href="{{ asset('backend/lib/font-awesome/css/font-awesome.css') }} " rel="stylesheet">
+    <link href="{{ asset('backend/lib/Ionicons/css/ionicons.css') }} " rel="stylesheet">
+    <link href="{{ asset('backend/lib/select2/css/select2.min.css') }} " rel="stylesheet">
+
+
+    <!-- Starlight CSS -->
+    <link rel="stylesheet" href="{{ asset('backend/css/starlight.css') }} ">
+</head>
+
+<body>
+
+    <div class="d-flex align-items-center justify-content-center bg-sl-primary ht-md-100v">
+
+        <div class="login-wrapper wd-300 wd-xs-400 pd-25 pd-xs-40 bg-white">
+            <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">Admin <span class="tx-info tx-normal">Panel</span></div>
+            <div class="tx-center mg-b-60">Registration</div>
+            <x-jet-validation-errors class="mb-4" />
+
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <div class="form-group">
+                    <label for="name">Full Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Enter your fullname" required autofocus autocomplete="name">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" placeholder="Enter your email" required>
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="password" required autocomplete="new-password" placeholder="Enter Password">
+                </div><!-- form-group -->
+                <div class="form-group">
+                    <label for="password_confirmation">Confirm Password</label>
+                    <input type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Password">
+                </div><!-- form-group -->
+                @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                <div class="mt-4">
+                    <x-jet-label for="terms">
+                        <div class="flex items-center">
+
+                            <div class="ml-2">
+                            <x-jet-checkbox name="terms" id="terms" />
+
+                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms').'</a>',
+                                'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                ]) !!}
+                            </div>
+                        </div>
+                    </x-jet-label>
+                </div>
+                @endif
+                <button type="submit" class="btn btn-info btn-block">Sign Up</button>
+            </form>
+            <div class="mg-t-40 tx-center">Already have an account? <a href="{{ route('login') }}" class="tx-info">Sign In</a></div>
+        </div><!-- login-wrapper -->
+    </div><!-- d-flex -->
+
+    <script src="{{ asset('backend/lib/jquery/jquery.js') }} "></script>
+    <script src="{{ asset('backend/lib/popper.js/popper.js') }} "></script>
+    <script src="{{ asset('backend/lib/bootstrap/bootstrap.js') }} "></script>
+    <script src="{{ asset('backend/lib/select2/js/select2.min.js') }} "></script>
+    <script>
+        $(function() {
+            'use strict';
+
+            $('.select2').select2({
+                minimumResultsForSearch: Infinity
+            });
+        });
+    </script>
+
+</body>
+
+</html>
