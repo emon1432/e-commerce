956b59530290e71325c5060e537f46da514a385a
emon1432 <ovirajemon143@gmail.com>
2022-05-07 16:04:38 +0600

    latest sql file upload

--- database/e_commerce.sql
+++ database/e_commerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 05, 2022 at 07:38 PM
+-- Generation Time: May 07, 2022 at 12:03 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -100,6 +100,34 @@ CREATE TABLE IF NOT EXISTS `coupons` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customers`
+--
+
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `current_team_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `profile_photo_path` varchar(2048) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `customers_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
+(1, 'customer1', 'customer1@gmail.com', '2022-04-03 08:09:12', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'WiBqGHjk78', NULL, NULL, '2022-04-03 08:09:12', '2022-04-03 08:09:12');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `failed_jobs`
 --
 
@@ -126,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -147,7 +175,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (12, '2022_01_17_171751_create_brands_table', 2),
 (13, '2022_02_03_123414_create_coupons_table', 3),
 (26, '2022_02_03_171141_create_subscribers_table', 4),
-(27, '2022_02_04_160841_create_products_table', 4);
+(27, '2022_02_04_160841_create_products_table', 4),
+(28, '2022_04_03_134707_create_customers_table', 5),
+(29, '2022_04_27_161302_create_wishlists_table', 6);
 
 -- --------------------------------------------------------
 
@@ -216,14 +246,15 @@ CREATE TABLE IF NOT EXISTS `products` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `category_id`, `subcategory_id`, `brand_id`, `product_name`, `product_code`, `product_quantity`, `product_details`, `product_color`, `product_size`, `selling_price`, `discount_price`, `video_link`, `main_slider`, `hot_deal`, `best_rated`, `mid_slider`, `hot_new`, `trend`, `image_one`, `image_two`, `image_three`, `status`, `created_at`, `updated_at`) VALUES
-(1, 18, 12, 12, 'Abcd', '123', '35', 'abcdefghijklmnopqrstuvwxyz', 'black', 'XL', '399', '299', 'http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=e_commerce&table=sub_categories&pos=0', 1, 1, 1, 1, 1, 1, 'images/product/1726027392677066.png', 'images/product/1726027392684446.jpeg', 'images/product/1726027392689718.jpeg', 1, NULL, '2022-02-28 11:08:37');
+(1, 18, 12, 12, 'Abcd', '123', '35', 'abcdefghijklmnopqrstuvwxyz', 'black', 'XL', '399', '100', 'http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=e_commerce&table=sub_categories&pos=0', 1, 1, 1, 1, 1, 1, 'images/product/1726027392677066.png', 'images/product/1726027392684446.jpeg', 'images/product/1726027392689718.jpeg', 1, NULL, '2022-04-28 15:00:34'),
+(2, 18, 11, 11, 'ABCD2', '12345', '15', '<div style=\"color: #f6f6f4;background-color: #282a36;font-family: \'Fira Code\', \'Droid Sans Mono\', \'monospace\', monospace;font-weight: normal;font-size: 14px;line-height: 19px;white-space: pre;\"><div><span style=\"color: #f6f6f4;\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae reiciendis autem neque nesciunt incidunt, suscipit, quisquam ullam quos obcaecati, tempore accusantium eaque molestiae. Nemo laboriosam harum, accusamus assumenda, asperiores aliquid consectetur necessitatibus, quidem maxime cumque odio. Ratione earum officia voluptates quod nisi reprehenderit soluta repellat suscipit ex quo accusantium pariatur, esse assumenda sequi? Optio saepe officia, repudiandae placeat deserunt quisquam ratione autem? Quam veniam nostrum, explicabo quae illum corporis blanditiis quaerat inventore repudiandae odio maxime? Quas dolore sit excepturi laudantium asperiores! Nesciunt id dolorum alias ipsam totam beatae illum, esse omnis animi sapiente aspernatur nostrum rem est consectetur corrupti repudiandae.</span></div></div>', 'Yellow', 'XL,L,M,S', '399', '299', 'http://localhost/phpmyadmin/index.php?route=/sql&db=e_commerce&table=users&pos=0', 1, 1, 1, 1, 1, 1, 'images/product/1730278770379436.jpeg', 'images/product/1730278770379667.jpeg', 'images/product/1730278770379734.jpeg', 1, '2022-04-16 09:20:23', '2022-04-16 09:29:44');
 
 -- --------------------------------------------------------
 
@@ -248,7 +279,12 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('8Bz6HDmgeSsiCqMGtsknlnEbG9jjLCAl9o72YtPB', 1, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiSmRIeEczbllEeUdUbFlkNHFRZEhTRHluWVpQZEVmZzVVRlFBOFVQdiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTAkMTFUZkR5VnJMaDNoQ0FTTTNUd3o2ZVpqOS9CRko2aFpVeDQ0ZlY1Z2RZT3cwcW5xa3pSTksiO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJDExVGZEeVZyTGgzaENBU00zVHd6NmVaajkvQkZKNmhaVXg0NGZWNWdkWU93MHFucWt6Uk5LIjt9', 1646156040);
+('4Zb0hkOgCzznB8OlGReJu1SunKemuIBzJIfjM3QX', NULL, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiTUhmaElEU1hWTEtxWW9na0VidXo1bkhRdTI4dUVJc3BLQkgyRlBnZSI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozNToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL3VzZXIvd2lzaGxpc3QiO31zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czozNToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL3VzZXIvd2lzaGxpc3QiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1651176176),
+('e2BdmBFgh5VPXUA73MjmzgTWl6OqJ692yah4atWR', NULL, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoibjB2RG1sMFBnbXRadDN4MnlIZmxpbFBPaFVNa3BPTFJyeXdlN1VKbSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1651175889),
+('EQScGgxNVlWOYf6nztyKlpuHP5OBqEkT9ufd9l5e', 1, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiUUFpdXJxQjcxaGxrUnFnd2F0alQ1WWdkMUlrVDRRZVJ2eXRXdmtxdiI7czozOiJ1cmwiO2E6MDp7fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjM0OiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvcHJvZHVjdHMvYWxsIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTtzOjE3OiJwYXNzd29yZF9oYXNoX3dlYiI7czo2MDoiJDJ5JDEwJFR5a2RWL09OVXRBczZLaFpSMVFPZy5SenFHeWhCRFBPOTFJMDdzMzZQL2lsU3R1UjZIdXdtIjtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCRUeWtkVi9PTlV0QXM2S2haUjFRT2cuUnpxR3loQkRQTzkxSTA3czM2UC9pbFN0dVI2SHV3bSI7fQ==', 1651179634),
+('OJ6S0wVefnlewXy8zQfZzVXifRuHRgoCzqO7v5Yy', NULL, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoieWtuYzdIcTIzUlBuY1RVZzNad1p4WUJYQWdYQmJwMnN2U2gyczRiWiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC91c2VyL2xvZ2luIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1651176176),
+('WMXggPeciTf5Osy1Lc4VTMpfSIv9J8pcz4FJQe1H', 5, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoiTFNjRXdKT3hXM0JTNWVhTERadmlNWU1lbk5jMlJpSWtGSFYwNVdWViI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6MzoidXJsIjthOjA6e31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTo1O3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTAkcUFTaTF3dHZuYTVTT0VWd3ZlWEw4ZUlrU2I2VEhZWXJ5T2luYlRKS0o2QzBuSHBhWTFxUXkiO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJHFBU2kxd3R2bmE1U09FVnd2ZVhMOGVJa1NiNlRIWVlyeU9pbmJUSktKNkMwbkhwYVkxcVF5Ijt9', 1651180165),
+('xSIvn6ABOtVKc5Eo4SCHnMyfvRPs0Z4lv3kc7p5C', 5, '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0', 'YTo3OntzOjY6Il90b2tlbiI7czo0MDoibFJVMk9DaFg0aXZ3cDBjRERXRzdYUkcwYzgxQ2N0eEJuelhMYnlxcSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6MzoidXJsIjthOjA6e31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aTo1O3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTAkcUFTaTF3dHZuYTVTT0VWd3ZlWEw4ZUlrU2I2VEhZWXJ5T2luYlRKS0o2QzBuSHBhWTFxUXkiO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJHFBU2kxd3R2bmE1U09FVnd2ZVhMOGVJa1NiNlRIWVlyeU9pbmJUSktKNkMwbkhwYVkxcVF5Ijt9', 1651262367);
 
 -- --------------------------------------------------------
 
@@ -395,9 +431,30 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `user_role`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
-(1, 'Md khairul Islam Emon', 'admin@gmail.com', NULL, '$2y$10$11TfDyVrLh3hCASM3Twz6eZj9/BFJ6hZUx44fV5gdYOw0qnqkzRNK', 1, NULL, NULL, NULL, 1, 'profile-photos/c2wOMg6fUl5ymOAjz15ABvUUckwZPIglslkmtzbK.jpg', '2022-01-15 03:57:01', '2022-02-26 11:17:37'),
-(4, 'admin1', 'admin1@gmail.com', NULL, '$2y$10$lw0m58DkoM.l4cilkK.qHeU78GLZuJ6Uwwmm86qLoVoIb0YXAhPOu', 1, NULL, NULL, NULL, NULL, NULL, '2022-02-28 11:20:45', '2022-02-28 11:20:45'),
-(5, 'Customer', 'customer@gmail.com', NULL, '$2y$10$qASi1wtvna5SOEVwveXL8eIkSb6THYYryOinbTJKJ6C0nHpaY1qQy', 0, NULL, NULL, NULL, NULL, NULL, '2022-02-28 11:26:04', '2022-02-28 11:26:04');
+(1, 'Md khairul Islam Emon', 'admin@gmail.com', NULL, '$2y$10$TykdV/ONUtAs6KhZR1QOg.RzqGyhBDPO91I07s36P/ilStuR6Huwm', 1, NULL, NULL, NULL, 1, 'profile-photos/c2wOMg6fUl5ymOAjz15ABvUUckwZPIglslkmtzbK.jpg', '2022-01-15 03:57:01', '2022-04-16 09:02:34'),
+(5, 'Customer', 'customer@gmail.com', NULL, '$2y$10$qASi1wtvna5SOEVwveXL8eIkSb6THYYryOinbTJKJ6C0nHpaY1qQy', 0, NULL, NULL, NULL, 5, NULL, '2022-02-28 11:26:04', '2022-04-16 10:24:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlists`
+--
+
+CREATE TABLE IF NOT EXISTS `wishlists` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `wishlists`
+--
+
+INSERT INTO `wishlists` (`id`, `user_id`, `product_id`, `created_at`, `updated_at`) VALUES
+(30, 5, 2, '2022-04-29 13:57:08', '2022-04-29 13:57:08');
 
 --
 -- Constraints for dumped tables
